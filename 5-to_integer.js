d5559779926e7bd470c14e57900fcb40a21fdc87
Jana <jana@gmail.com>
2025-05-25 23:58:52 +0300

    tasks

---
+++ 5-to_integer.js
@@ -0,0 +1,8 @@
+const arg = process.argv[2];
+const number = Number(arg);
+
+if (!isNaN(number)) {
+  console.log(`My number: ${parseInt(number)}`);
+} else {
+  console.log("Not a number");
+}
---
+++ 9-add.js
@@ -0,0 +1,14 @@
+
+function add(a, b) {
+  return a + b;
+}
+
+const args = process.argv.slice(2);
+const a = parseInt(args[0]);
+const b = parseInt(args[1]);
+
+if (isNaN(a) || isNaN(b)) {
+  console.log("Invalid input: both arguments must be integers");
+} else {
+  console.log(add(a, b));
+}
