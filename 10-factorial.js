1e882f74afb22043499e44612d4d1a9e862e0781
Jana Elsalhy <janamohamed@Janas-MacBook-Pro.local>
2025-06-27 20:29:41 +0300

    k

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,8 +1,10 @@
 const n = Number(process.argv[2]);
 
 function factorial(n) {
-    if (isNaN(n)|| n==0 ){
-        console.log(1);
+    if (isNaN(n)){
+        return 1;
+    }else if (n === 0) {
+        return 1;
     }else{
         let result=n*factorial(n-1)
         return result;
